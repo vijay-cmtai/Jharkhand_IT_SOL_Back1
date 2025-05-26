2d3fcd5db7dbd9442d719a681fa09fe965ba48be
vijay-cmtai <vijay.maurya@completrix.com>
2025-05-26 11:58:19 +0530

    Update applicationRoutes.js

--- Routes/applicationRoutes.js
+++ Routes/applicationRoutes.js
@@ -7,10 +7,12 @@ const {
   getApplicationById,
   updateApplicationStatus,
   deleteApplication,
-} = require("../controllers/Application");
-const { uploadResume } = require("../middleware/multerUpload"); // Make sure path is correct
+} = require("../controllers/application"); // Corrected path if controller name was applicationController.js
+const { uploadResume } = require("../middleware/multerUpload");
 
+// The field name "resume" here MUST match the field name used in FormData on the frontend
 router.post("/upload", uploadResume.single("resume"), submitApplication);
+
 router.get("/applications", getAllApplications);
 router.get("/applications/:id", getApplicationById);
 router.put("/applications/:id/status", updateApplicationStatus);
