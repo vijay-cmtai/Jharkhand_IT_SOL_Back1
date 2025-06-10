1c75d985f313e08ba7327e71c4ab0dc8c9db3b87
vijay-cmtai <vijay.maurya@completrix.com>
2025-06-10 21:54:02 +0530

    Update portfolioRoutes.js

--- Routes/portfolioRoutes.js
+++ Routes/portfolioRoutes.js
@@ -1,13 +1,29 @@
 // Routes/portfolioRoutes.js
+
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/upload");
+
+// Import all controller functions
 const {
   createPortfolio,
   getAllPortfolios,
+  updatePortfolio,
+  deletePortfolio,
 } = require("../controllers/portfolio");
 
+// @route   POST /portfolio/create
+// Creates a new portfolio item
 router.post("/create", upload.single("image"), createPortfolio);
+
+// @route   GET /portfolio/all
+// Gets all portfolio items
 router.get("/all", getAllPortfolios);
+router.put("/:id", upload.single("image"), updatePortfolio);
+
+// @route   DELETE /portfolio/:id
+// Deletes a specific portfolio item
+router.delete("/:id", deletePortfolio);
+
 
 module.exports = router;
